df0d8ce5314f798379ec951dcd79e22b8a06746a
chase <chasze@gmail.com>
2021-01-23 16:37:07 -0600

    new commit added more ejs

--- index.js
+++ index.js
@@ -7,14 +7,27 @@ const app= express();
 const path = require('path');
 
 app.set('view engine', 'ejs');
+app.set('views' , path.join(__dirname, '/views') )
+
 
 app.get('/' , (req, res) => {
-    res.render('home.ejs')
+    res.render('home')
 })
+
+// : --- the colon is a template for anything foloowing /r/  
+app.get('/r/:subreddit' , (req, res) => {
+    const {subreddit} = req.params;
+
+    res.render('subreddit', {subreddit});
+});
 
 
 app.get('/rand' , (req, res) => {
-    res.render('random.ejs')
+
+   const num =  Math.floor(Math.random() *10) + 1
+                            // number value is passed and can be used as name num to template
+    res.render('random', {rand: num})
+    //renders the ejs random page
 })
 
 
